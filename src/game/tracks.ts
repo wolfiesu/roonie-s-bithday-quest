b398fc3a24dde7042e4f7909413f80e1cf91541f
wolfiesu <esrss1803@gmail.com>
2026-08-12 05:14:36 +0200

    fix: sync exact filenames including arabic track path

--- src/game/tracks.ts
+++ src/game/tracks.ts
@@ -8,12 +8,27 @@ export const TRACKS: Track[] = [
   {
     title: "شيوكلونك شكر",
     artist: "بنوته خبله",
-    src: "/music/shokolatak.m4a",
+    src: "/music/بنوته_خبله_اغنية_شيوكلونك_شكر_شنو_طالع_كمر_.m4a",
   },
   {
-    title: "I Saw Your Face",
+    title: "Ladygirl",
     artist: "Malcolm Todd",
-    src: "/music/01. Malcolm Todd - I Saw Your Face.mp3",
+    src: "/music/05 - Malcolm Todd - Ladygirl.mp3",
+  },
+  {
+    title: "Maggots For Brains",
+    artist: "Olivia Rodrigo",
+    src: "/music/04. Olivia Rodrigo - maggots for brains.mp3",
+  },
+  {
+    title: "Breathe Deeper",
+    artist: "Tame Impala",
+    src: "/music/05 - Tame Impala - Breathe Deeper.mp3",
+  },
+  {
+    title: "I Really Want to Stay At Your House",
+    artist: "Rosa Walton",
+    src: "/music/05 - Rosa Walton - I Really Want to Stay ...mp3",
   },
   {
     title: "Lights",
@@ -31,23 +46,8 @@ export const TRACKS: Track[] = [
     src: "/music/02. Olivia Rodrigo - stupid song.mp3",
   },
   {
-    title: "Maggots For Brains",
-    artist: "Olivia Rodrigo",
-    src: "/music/04 - Olivia Rodrigo - maggots for brains.mp3",
-  },
-  {
-    title: "Ladygirl",
+    title: "I Saw Your Face",
     artist: "Malcolm Todd",
-    src: "/music/05 - Malcolm Todd - Ladygirl.mp3",
-  },
-  {
-    title: "I Really Want to Stay At Your House",
-    artist: "Rosa Walton",
-    src: "/music/05 - Rosa Walton - I Really Want to Stay ...mp3",
-  },
-  {
-    title: "Breathe Deeper",
-    artist: "Tame Impala",
-    src: "/music/05 - Tame Impala - Breathe Deeper.mp3",
+    src: "/music/01. Malcolm Todd - I Saw Your Face.mp3",
   },
 ];
