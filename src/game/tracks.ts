2f421bd9e2fee0ab50f3d25b9ece8c98808e07fa
wolfiesu <esrss1803@gmail.com>
2026-08-12 05:36:29 +0200

    Fix audio file paths and clean filenames

--- src/game/tracks.ts
+++ src/game/tracks.ts
@@ -1,53 +1,47 @@
-export type Track = {
-  title: string;
-  artist: string;
-  src: string;
-};
-
 export const TRACKS: Track[] = [
   {
     title: "شيوكلونك شكر",
     artist: "بنوته خبله",
-    src: "/music/بنوته_خبله_اغنية_شيوكلونك_شكر_شنو_طالع_كمر_.m4a",
+    src: "music/shoklonk.m4a",
   },
   {
     title: "Ladygirl",
     artist: "Malcolm Todd",
-    src: "/music/ladygirl.mp3",
+    src: "music/ladygirl.mp3",
   },
   {
     title: "Maggots For Brains",
     artist: "Olivia Rodrigo",
-    src: "/music/04. Olivia Rodrigo - maggots for brains.mp3",
+    src: "music/maggots-for-brains.mp3",
   },
   {
     title: "Breathe Deeper",
     artist: "Tame Impala",
-    src: "/music/05 - Tame Impala - Breathe Deeper.mp3",
+    src: "music/breathe-deeper.mp3",
   },
   {
     title: "I Really Want to Stay At Your House",
     artist: "Rosa Walton",
-    src: "/music/05 - Rosa Walton - I Really Want to Stay at Your House.mp3",
+    src: "music/i-really-want-to-stay.mp3",
   },
   {
     title: "Lights",
     artist: "BTS",
-    src: "/music/01 - BTS - Lights.mp3",
+    src: "music/bts-lights.mp3",
   },
   {
     title: "Harvey",
     artist: "Her's",
-    src: "/music/01 - Her's - Harvey.mp3",
+    src: "music/hers-harvey.mp3",
   },
   {
     title: "Stupid Song",
     artist: "Olivia Rodrigo",
-    src: "/music/02. Olivia Rodrigo - stupid song.mp3",
+    src: "music/stupid-song.mp3",
   },
   {
     title: "I Saw Your Face",
     artist: "Malcolm Todd",
-    src: "/music/01. Malcolm Todd - I Saw Your Face.mp3",
+    src: "music/i-saw-your-face.mp3",
   },
 ];
