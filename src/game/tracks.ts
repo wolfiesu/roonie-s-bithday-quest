4d7fd2dd3cc445e4ba11e9f09f43862930b77871
wolfiesu <esrss1803@gmail.com>
2026-08-12 05:43:06 +0200

    Move music to src and update track imports

--- src/game/tracks.ts
+++ src/game/tracks.ts
@@ -1,47 +1,63 @@
+import shoklonkMusic from "../music/shoklonk.m4a";
+import ladygirlMusic from "../music/ladygirl.mp3";
+import maggotsMusic from "../music/maggots-for-brains.mp3";
+import breatheMusic from "../music/breathe-deeper.mp3";
+import stayMusic from "../music/i-really-want-to-stay.mp3";
+import lightsMusic from "../music/bts-lights.mp3";
+import harveyMusic from "../music/hers-harvey.mp3";
+import stupidMusic from "../music/stupid-song.mp3";
+import faceMusic from "../music/i-saw-your-face.mp3";
+
+export type Track = {
+  title: string;
+  artist: string;
+  src: string;
+};
+
 export const TRACKS: Track[] = [
   {
     title: "شيوكلونك شكر",
     artist: "بنوته خبله",
-    src: "music/shoklonk.m4a",
+    src: shoklonkMusic,
   },
   {
     title: "Ladygirl",
     artist: "Malcolm Todd",
-    src: "music/ladygirl.mp3",
+    src: ladygirlMusic,
   },
   {
     title: "Maggots For Brains",
     artist: "Olivia Rodrigo",
-    src: "music/maggots-for-brains.mp3",
+    src: maggotsMusic,
   },
   {
     title: "Breathe Deeper",
     artist: "Tame Impala",
-    src: "music/breathe-deeper.mp3",
+    src: breatheMusic,
   },
   {
     title: "I Really Want to Stay At Your House",
     artist: "Rosa Walton",
-    src: "music/i-really-want-to-stay.mp3",
+    src: stayMusic,
   },
   {
     title: "Lights",
     artist: "BTS",
-    src: "music/bts-lights.mp3",
+    src: lightsMusic,
   },
   {
     title: "Harvey",
     artist: "Her's",
-    src: "music/hers-harvey.mp3",
+    src: harveyMusic,
   },
   {
     title: "Stupid Song",
     artist: "Olivia Rodrigo",
-    src: "music/stupid-song.mp3",
+    src: stupidMusic,
   },
   {
     title: "I Saw Your Face",
     artist: "Malcolm Todd",
-    src: "music/i-saw-your-face.mp3",
+    src: faceMusic,
   },
 ];
